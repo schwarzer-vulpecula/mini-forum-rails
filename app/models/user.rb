11c0118c5e82a6319274566288837ce74346837c
Schwarzer Vulpecula <schwarzer.vulpecula@gmail.com>
2021-06-06 18:04:44 +1000

    Added password length constraints

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   validates :username, format: { with: /\A[a-zA-Z0-9]+(-[a-zA-Z0-9]+)*\z/ }
-  validates :password, confirmation: true, presence: true
+  validates :password, confirmation: true, length: { minimum: 8 }
 end
