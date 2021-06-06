5e08ff7a23c485e2d58a66ac28f7d69c088f32b7
Schwarzer Vulpecula <schwarzer.vulpecula@gmail.com>
2021-06-06 19:55:48 +1000

    Added salts to password hashes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,18 @@
+require 'securerandom'
 require 'digest'
 
 class User < ApplicationRecord
   validates :username, format: { with: /\A[a-zA-Z0-9]+(-[a-zA-Z0-9]+)*\z/ }, length: { minimum: 3, maximum: 25}, uniqueness: true
   validates :password, confirmation: true, length: { minimum: 8, maximum: 24 }
 
-  after_validation :hash_password, on: [ :create, :update ]
+  before_save :hash_password
 
   private
-    # Hashes the password, so that it is not stored in plain text
+    # Hashes the password, using salts, so that it is not stored in plain text
     # This is not the most secure implementation
     def hash_password
-      self.password = Digest::SHA256.hexdigest password
+      self.salt = SecureRandom.alphanumeric(10)
+      self.password << self.salt
+      self.password = Digest::SHA256.hexdigest self.password
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_06_084634) do
+ActiveRecord::Schema.define(version: 2021_06_06_095127) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_06_06_084634) do
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "salt"
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
