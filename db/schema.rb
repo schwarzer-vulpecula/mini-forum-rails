7b91d4988213a91ac9edbd9f0b55510ed2098d4e
Schwarzer Vulpecula <schwarzer.vulpecula@gmail.com>
2021-06-06 17:05:46 +1000

    Generated scaffold for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_05_071030) do
+ActiveRecord::Schema.define(version: 2021_06_06_070127) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
