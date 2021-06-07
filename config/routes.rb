9e25713105bb6938d68e4a06f2c90c3cf0ac439e
Schwarzer Vulpecula <schwarzer.vulpecula@gmail.com>
2021-06-07 16:28:46 +1000

    Generated session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :posts
   root "home#index"
+  resources :posts
+  resources :users
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
