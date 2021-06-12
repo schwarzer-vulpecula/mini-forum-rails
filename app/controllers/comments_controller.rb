b2b26caed9e3ae03ed3e92c2e1abb42058b3a5ab
Schwarzer Vulpecula <schwarzer.vulpecula@gmail.com>
2021-06-12 19:32:49 +1000

    Add comment section below posts, but no display yet

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,7 +20,7 @@ class CommentsController < ApplicationController
         format.html { redirect_to @comment, notice: "Comment was successfully created." }
         format.json { render :show, status: :created, location: @comment }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { redirect_to @comment.post, alert: "Comment cannot be empty." }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
